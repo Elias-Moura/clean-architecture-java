8b758bf669b4be8ea31cd955168f3893cd5680ab
Elias Moura <elias.smourayt@gmail.com>
2023-12-01 13:31:37 -0300

    Classe para representar um telefone.

---
+++ src/main/java/escola/Telefone.java
@@ -0,0 +1,24 @@
+package escola;
+
+public class Telefone {
+    private String ddd;
+    private String numero;
+
+    public Telefone(String ddd, String numero) {
+        if (ddd == null || !ddd.matches("^\\d{2}$") || ddd == ""){
+            throw new IllegalArgumentException("DDD inválido");
+        } else if (numero == null || !numero.matches("^\\d{8,9}$") || numero == "") {
+            throw new IllegalArgumentException("Número inválido");
+        }
+        this.ddd = ddd;
+        this.numero = numero;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+}

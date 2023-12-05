f0a55512d1ba8b4cbc5eb94d6e156a072c2c0f41
Elias Moura <elias.smourayt@gmail.com>
2023-12-05 13:33:34 -0300

    Exception personalizada para representar a regra de negócio em que o aluno não pode ter mais do que dois telefones.

---
+++ src/main/java/br/com/alura/escola/dominio/aluno/LimiteDeTelefonesException.java
@@ -0,0 +1,8 @@
+package br.com.alura.escola.dominio.aluno;
+
+public class LimiteDeTelefonesException extends RuntimeException {
+
+    public LimiteDeTelefonesException(String s) {
+        super(s);
+    }
+}

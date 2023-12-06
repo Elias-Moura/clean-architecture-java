f7265a2a386f270d9c3a460aa3b914c71e010fd3
Elias Moura <elias.smourayt@gmail.com>
2023-12-06 13:46:02 -0300

    Adicionando a camada de eventos.

--- src/main/java/br/com/alura/escola/aplicacao/matricular/MatricularAluno.java
+++ src/main/java/br/com/alura/escola/aplicacao/matricular/MatricularAluno.java
@@ -1,20 +1,26 @@
 package br.com.alura.escola.aplicacao.matricular;
 
 import br.com.alura.escola.dominio.aluno.Aluno;
+import br.com.alura.escola.dominio.aluno.AlunoMatriculado;
+import br.com.alura.escola.dominio.aluno.PublicadorDeEventos;
 import br.com.alura.escola.dominio.aluno.RepositorioDeAlunos;
 
 
 //UseCase
 public class MatricularAluno {
     private final RepositorioDeAlunos repositorio;
+    private final PublicadorDeEventos publicador;
 
-    public MatricularAluno(RepositorioDeAlunos repositorio) {
+    public MatricularAluno(RepositorioDeAlunos repositorio, PublicadorDeEventos publicador) {
         this.repositorio = repositorio;
+        this.publicador = publicador;
     }
 
     // Pattern command
     public void executa(MatricularAlunoDTO aluno){
         Aluno novo = aluno.criarAluno();
         repositorio.matricular(novo);
+        AlunoMatriculado evento = new AlunoMatriculado(novo.getCpf());
+        publicador.publicar(evento);
     }
 }

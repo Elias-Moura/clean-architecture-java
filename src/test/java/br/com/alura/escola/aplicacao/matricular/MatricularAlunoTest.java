904ca2c80e8c4854d01f0a8a155e4447ae8dbe9a
Elias Moura <elias.smourayt@gmail.com>
2023-12-06 13:44:42 -0300

    Corrigindo testes.

--- src/test/java/br/com/alura/escola/aplicacao/matricular/MatricularAlunoTest.java
+++ src/test/java/br/com/alura/escola/aplicacao/matricular/MatricularAlunoTest.java
@@ -2,6 +2,8 @@ package br.com.alura.escola.aplicacao.matricular;
 
 import br.com.alura.escola.dominio.aluno.Aluno;
 import br.com.alura.escola.dominio.aluno.CPF;
+import br.com.alura.escola.dominio.aluno.LogDeAlunoMatriculado;
+import br.com.alura.escola.dominio.aluno.PublicadorDeEventos;
 import br.com.alura.escola.infra.aluno.RepositorioDeAlunosEmMemoria;
 import org.junit.jupiter.api.Test;
 
@@ -11,8 +13,11 @@ public class MatricularAlunoTest {
 
     @Test
     void alunoDeveriaSerPersistido() {
-        RepositorioDeAlunosEmMemoria repositorio = new RepositorioDeAlunosEmMemoria();
-        MatricularAluno useCase = new MatricularAluno(repositorio);
+        var repositorio = new RepositorioDeAlunosEmMemoria();
+        var publicador = new PublicadorDeEventos();
+        publicador.adicionar(new LogDeAlunoMatriculado());
+
+        var useCase = new MatricularAluno(repositorio, publicador);
 
         MatricularAlunoDTO dados = new MatricularAlunoDTO(
                 "Ciclonildo",
@@ -24,7 +29,7 @@ public class MatricularAlunoTest {
         Aluno encontrado = repositorio.buscarPorCPF(new CPF("123.456.789-00"));
 
         assertEquals("Ciclonildo", encontrado.getNome());
-        assertEquals("123.456.789-00", encontrado.getCpf());
+        assertEquals("123.456.789-00", encontrado.getCpf().toString());
         assertEquals("ciclonildo@gmail.com", encontrado.getEmail());
 
 
